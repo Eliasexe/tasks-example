2eed77fd27d59f6eb7e6a92e355c97b7b6b418af
Rafael <rafacotrina720@gmail.com>
2023-02-08 20:07:12 -0500

    first commit

---
+++ src/task.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Task Title",
+    description: "Task Description",
+  },
+  {
+    id: 1,
+    title: "Task Title 2",
+    description: "Task Description 2",
+  },
+  {
+    id: 2,
+    title: "Task Title 3",
+    description: "Task Description 3",
+  },
+];
